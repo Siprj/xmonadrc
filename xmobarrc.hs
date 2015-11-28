6029c15995cd97d228b56eecd260c070fcc64e03
Jan Sipr <sipr.jan@gmail.com>
2015-11-28 19:56:00 +0100

    Add xmonbar cofiguration

---
+++ xmobarrc.hs
@@ -0,0 +1,22 @@
+Config { font = "-*-Fixed-Bold-R-Normal-*-13-*-*-*-*-*-*-*"
+        , borderColor = "black"
+        , border = TopB
+        , bgColor = "black"
+        , fgColor = "grey"
+        , position = TopW L 100
+        , allDesktops = True
+        , commands = [ Run Weather "CYVR" ["-t","<tempC>C","-L","18","-H","25","--normal","green","--high","red","--low","lightblue"] 36000
+                        , Run Network "enp4s0" ["-L","0","-H","32","--normal","green","--high","red"] 10
+                        , Run Network "wlp7s0" ["-L","0","-H","32","--normal","green","--high","red"] 10
+                        , Run Cpu ["-L","3","-H","50","--normal","green","--high","red"] 10
+                        , Run Memory ["-t","Mem: <usedratio>%"] 10
+                        , Run Swap [] 10
+                        , Run Com "uname" ["-s","-r"] "" 36000
+                        , Run Date "%a %b %_d %Y %H:%M:%S" "date" 10
+                        , Run StdinReader
+                        , Run Kbd [("cz", "cz"), ("us", "us")]
+                        ]
+        , sepChar = "%"
+        , alignSep = "}{"
+        , template = "%StdinReader% | %cpu% | %memory% * %swap% | %enp4s0% - %wlp7s0% }{%kbd% <fc=#ee9a00>%date%</fc> | %uname% | %CYVR% "
+        }
