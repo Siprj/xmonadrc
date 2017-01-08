2eff925e5417c16632dedce7dddc90b159aeb380
Jan Sipr <sipr.jan@gmail.com>
2017-01-08 16:51:59 +0100

    Work in progress

--- xmonad.hs
+++ xmonad.hs
@@ -1,81 +1,53 @@
-import Control.Concurrent (forkIO, threadDelay, Chan, newChan, writeChan, readChan)
+import Control.Concurrent (forkOS, threadDelay, Chan, newChan, writeChan, readChan)
 import Control.Monad (forever, void)
+import qualified Data.Map as M
+import Data.Char (toLower)
+import Data.IORef
+import Data.List (isInfixOf)
 import Data.Monoid ((<>))
 import System.Exit
 import System.IO
 import System.Posix.Process
 import System.Process
 
-import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.ManageHelpers
-import qualified XMonad.Util.EntryHelper as EH
 import Graphics.X11.ExtraTypes.XF86
     ( xF86XK_AudioRaiseVolume
     , xF86XK_AudioLowerVolume
     )
-import XMonad.Util.Run (spawnPipe)
-import XMonad.Util.EZConfig (additionalKeys)
-import XMonad.Wallpaper
+import XMonad
+import XMonad.Actions.PhysicalScreens
+import XMonad.Actions.SpawnOn
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.UrgencyHook
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.EwmhDesktops (ewmh, fullscreenEventHook)
+import qualified XMonad.Util.EntryHelper as EH
+import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.Run (spawnPipe)
+import XMonad.Util.Types (Direction2D(U))
+import XMonad.Wallpaper
 import XMonad.Wallpaper.Expand (expand)
 
 xmonadSandbox = "$HOME/xmonadrc/"
 xmonadBinDir = "$HOME/bin/"
 wallpaperDirectories = ["$HOME/Dropbox/Wallpapers/"]
 
--- {{{ Make compile and restart options work in sandbox
-
-main = EH.withCustomHelper conf
-  where
-    conf = EH.defaultConfig
-        { EH.run = oldMain
-        , EH.compile = compileInSandbox
-        , EH.postCompile = postCompile
-        }
-
-compileInSandbox :: Bool -> IO (Either String ())
-compileInSandbox _ = do
-    ep <- expand xmonadSandbox
-    xb <- expand xmonadBinDir
-    (_, _, errHandler, procHandler) <- createProcess
-        (proc "cabal" ["install", "--symlink-bindir", xb])
-            { cwd = Just ep
-            , std_err = CreatePipe
-            }
-    exitCodeToEather errHandler =<<  waitForProcess procHandler
-exitCodeToEather :: Maybe Handle -> ExitCode -> IO (Either String ())
-exitCodeToEather _ ExitSuccess = return $ Right ()
-exitCodeToEather mh (ExitFailure _) =
-    case mh of
-        Nothing -> return $ Left "ERROR: Can't retrieve error message from build command."
-        Just h  -> Left <$> hGetContents h
-
-postCompile :: Either String () -> IO ()
-postCompile (Right _)= return ()
-postCompile (Left errMsg) = do
-    putStrLn errMsg
-    void . forkProcess $ executeFile "xmessage" True ["-default", "okay", errMsg] Nothing
-
--- }}} Make compile and restart options work in sandbox
-
 -- {{{ Wallpaper setup
 
 randomWallpaper :: IO ()
-randomWallpaper = forever $ do
+randomWallpaper = do
     setRandomWallpaper wallpaperDirectories
-    threadDelay 3600000000
+    forever $ do
+        setRandomWallpaper wallpaperDirectories
+        threadDelay 3600000000
 
 -- }}} Wallpaper setup
 
 -- {{{ Keyboard layout switching
 
-newKeyboardHandling :: [String] -> IO (Chan ())
-newKeyboardHandling k = do
-    c <- newChan
-    forkIO $ keyboardLoop k c
-    return c
+newKeyboardHandling :: [String] -> IO (IORef [String])
+newKeyboardHandling k = newIORef k
 
 rotateKeyboardLayouts :: [String] -> [String]
 rotateKeyboardLayouts [] = []
@@ -86,38 +58,53 @@ safeHead :: a -> [a] -> a
 safeHead d (x:_) = x
 safeHead d _ = d
 
-keyboardLoop :: [String] -> Chan () -> IO ()
-keyboardLoop k c = do
-    readChan c
+keyboardLoop :: IORef [String] -> IO ()
+keyboardLoop c = do
+    k <- readIORef c
     spawn $ "setxkbmap " <> safeHead "us" k
-    keyboardLoop (rotateKeyboardLayouts k) c
+    writeIORef c $ rotateKeyboardLayouts k
 
-switchKeyboardLayout :: Chan () -> IO ()
-switchKeyboardLayout c =
-    writeChan c ()
+switchKeyboardLayout :: IORef [String] -> IO ()
+switchKeyboardLayout c = do
+    keyboardLoop c
 
 -- }}} Keyboard layout switching
 
-oldMain = do
-    forkIO $ randomWallpaper
-    kbl <- newKeyboardHandling ["cz", "us"]
-    xmproc <- spawnPipe "xmobar ~/xmonadrc/xmobarrc.hs"
+myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+    [((modm .|. mask, key), f sc)
+    | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+    , (f, mask) <- [(viewScreen, 0), (sendToScreen, shiftMask)]
+    ]
 
-    xmonad $ ewmh defaultConfig
-        { manageHook = manageDocks <+> (className =? "vlc" --> doFullFloat)  <+> (isFullscreen --> doFullFloat) <+> manageHook defaultConfig
-        , layoutHook = avoidStruts $ layoutHook defaultConfig
-        , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 50
-                        }
-        , handleEventHook = fullscreenEventHook
+main = do
+    kbl <- newKeyboardHandling ["cz", "us"]
+--    setRandomWallpaper wallpaperDirectories
+    forkOS $ randomWallpaper
+    cfg <- statusBar xmobarCmd xmobarPP' hidStatusBarShortcut . withUrgencyHook NoUrgencyHook $ ewmh def
+        { manageHook = manageDocks <> (className =? "vlc" --> doFullFloat)  <> (isFullscreen --> doFullFloat) <> manageHook def <> manageSpawn
+        , layoutHook = avoidStrutsOn [U] $ layoutHook def
+        , handleEventHook = fullscreenEventHook <> docksEventHook
         , modMask = mod4Mask
+        , keys = myKeys <> keys def
+        , startupHook = do
+            spawnOn "1" "xterm"
+            spawnOn "3" "firefox"
+            spawnOn "8" "quassel"
         } `additionalKeys`
             [ ((mod1Mask, xK_z), spawn "slock")
-            , ((0, xF86XK_AudioLowerVolume), spawn $ "amixer -c 1 set Master 1-")
-            , ((0, xF86XK_AudioRaiseVolume), spawn "amixer -c 1 set Master 1+")
-            , ((mod4Mask, 0x1008FF11), spawn "amixer set Master 3-")
-            , ((mod4Mask, 0x1008FF13), spawn "amixer set Master 3+")
-            , ((0, 0x1008FF12), spawn "amixer -D pulse set Master toggle")
+            , ((0, xF86XK_AudioLowerVolume), spawn "amixer -D default set Master 5%-")
+            , ((0, xF86XK_AudioRaiseVolume), spawn "amixer -D default set Master 5%+")
+            , ((mod4Mask, 0x1008FF11), spawn "amixer -D default set Master 1%-")
+            , ((mod4Mask, 0x1008FF13), spawn "amixer -D default set Master 1%+")
+            , ((0, 0x1008FF12), spawn "amixer -D defalt set Master toggle")
             , ((mod1Mask, xK_Shift_L), liftIO $ switchKeyboardLayout kbl)
             ]
+    xmonad cfg
+  where
+    xmobarPP' = xmobarPP
+        { ppTitle = xmobarColor "green" "" . shorten 50
+        , ppUrgent = xmobarColor "red" ""
+        }
+    xmobarCmd = "xmobar ~/xmonadrc/xmobarrc.hs"
+    hidStatusBarShortcut XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
